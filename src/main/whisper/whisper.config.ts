a496403cff1a7e17553c6e06e05b84c3ca98ed8e
madamroger007 <adamsetiadijr07@gmail.com>
2026-01-18 00:05:50 +0700

    chore: update file permissions for multiple files to executable

--- src/main/whisper/whisper.config.ts
+++ src/main/whisper/whisper.config.ts
@@ -5,14 +5,14 @@ const isWin = process.platform === 'win32';
 export const WHISPER_CONFIG = {
   binaryPath: getResourcePath(
     'whisper',
-    'bin',
+    isWin ? 'bin32' : 'bin',
     isWin ? 'whisper-cli.exe' : 'whisper-cli'
   ),
 
   modelPath: getResourcePath(
     'whisper',
     'models',
-    'ggml-large-v3-turbo.bin'
+    'ggml-small.bin'
   ),
 
   language: 'auto',
